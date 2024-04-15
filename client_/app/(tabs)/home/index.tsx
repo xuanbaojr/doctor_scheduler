595fbc174ec7782627e2b903642c7c8219d9f73f
DoanManhDuong <21020295@vnu.edu.vn>
2024-04-15 22:13:31 +0700

    create login and register

--- client_/app/(tabs)/home/index.tsx
+++ client_/app/(tabs)/home/index.tsx
@@ -1,33 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Link, Stack, } from 'expo-router'
-import ButtonDesign from '@/components/card/ButtonDesign'
-import ReviewDoctor from '@/components/card/ReviewDoctor'
-import Activity from '@/components/card/Activity'
+import { View, Text } from "react-native";
+import React from "react";
+import { Link, Stack } from "expo-router";
+import ButtonDesign from "@/components/card/ButtonDesign";
+import ReviewDoctor from "@/components/card/ReviewDoctor";
+import Activity from "@/components/card/Activity";
+import { useAuth } from "@clerk/clerk-expo";
 
-const HomePage = () => {
+const StartPage = () => {
+  const { isLoaded, userId, sessionId, getToken } = useAuth();
+  console.log("StartPage", isLoaded, userId, sessionId, getToken);
   return (
     <>
-    <Stack.Screen  options={{
-      headerShown : true,
-    }}/>
-    <View className="flex-col justify-between h-full bg-bg">
-      <View className='h-[40rem] '>
-        <View className='w-full absolute'>
-          
+      <Stack.Screen
+        options={{
+          headerShown: true,
+        }}
+      />
+      <View className="flex-col justify-between h-full bg-bg">
+        <View className="h-[40rem] ">
+          <View className="w-full absolute"></View>
+          <Activity />
         </View>
-        <Activity />
-      </View>
-      
-      <View className='b-0 h-3/5'>
-        <ReviewDoctor />
-      </View>
 
-        
+        <View className="b-0 h-3/5">
+          <ReviewDoctor />
+        </View>
       </View>
-
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default StartPage;
--- client_/app/_layout.tsx
+++ client_/app/_layout.tsx
@@ -1,23 +1,56 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Slot, Stack } from 'expo-router'
-import BottomBar from '../components/share/bottombar'
+import { useEffect } from "react";
+import * as SecureStore from "expo-secure-store";
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { Slot, useRouter, useSegments } from "expo-router";
 
-const RootLayout = () => {
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+// Cache the Clerk JWT
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
+const InitialLayout = () => {
+  const { isLoaded, isSignedIn } = useAuth();
+  const router = useRouter();
+
+  // If the user is signed in, redirect them to the home page
+  // If the user is not signed in, redirect them to the login page
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    if (isSignedIn) {
+      router.replace("/home");
+    } else if (!isSignedIn) {
+      router.replace("/login");
+    }
+  }, [isSignedIn]);
+
+  return <Slot />;
+};
+
+const RootLayoutNav = () => {
   return (
-    <>
-        <Stack>
-          <Stack.Screen 
-            name='(tabs)' 
-            options={{
-              headerShown: false,
-              
-            }}/>
-        </Stack>
-        
-    </>
-      
-  )
-}
+    <ClerkProvider
+      publishableKey={CLERK_PUBLISHABLE_KEY!}
+      tokenCache={tokenCache}
+    >
+      <InitialLayout />
+    </ClerkProvider>
+  );
+};
 
-export default RootLayout
+export default RootLayoutNav;
